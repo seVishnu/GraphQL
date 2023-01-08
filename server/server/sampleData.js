53e8ac2b514d013fcf4d7caa0fa624bf809677d8
vishnuSekhar <vishnusekhar96@gmail.com>
2023-01-08 18:03:01 +0530

    fea:Add graphiql and schema

---
+++ server/server/sampleData.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description: "Lorem Ipsum",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "eCommerce Website",
+    description: "Lorem Ipsum",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "eCommerce Website",
+    description: "Lorem Ipsum",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "walter",
+    email: "test@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "2",
+    name: "jesse",
+    email: "test@example.com",
+    phone: "1234567890",
+  },
+  {
+    id: "3",
+    name: "hank",
+    email: "test@example.com",
+    phone: "1234567890",
+  },
+];
+
+module.exports = { projects, clients };
